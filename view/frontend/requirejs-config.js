387dd5f55eeffe4834b7dce487372ef57fab18fc
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2020-08-20 10:22:25 +0200

    Add frontent support for asynchronous fetching of prices

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Swatches/js/swatch-renderer': {
+                'MageSuite_PerformanceProduct/js/swatch-async-prices': true,
+            },
+            'MageSuite_ServerSideSwatches/js/swatch-renderer': {
+                'MageSuite_PerformanceProduct/js/swatch-async-prices': true,
+            },
+        },
+    },
+};
