0691af18a7b1f20708ca2fd0097e5623c40ad173
Blazej Doleska <blazej.doleska@creativestyle.pl>
2024-09-18 13:57:07 +0200

    added additional config to include child product images for swatches

--- Helper/Configuration.php
+++ Helper/Configuration.php
@@ -5,6 +5,7 @@ namespace MageSuite\PerformanceProduct\Helper;
 class Configuration
 {
     public const XML_PATH_SWATCHES_ASYNC_OPTION_PRICES = 'product_performance/swatches/async_option_prices';
+    public const XML_PATH_SWATCHES_PRELOAD_CHILD_PRODUCTS_IMAGES = 'product_performance/swatches/preload_child_products_images';
 
     public const XML_PATH_CACHE_ATTRIBUTE_TEXT_VALUES = 'product_performance/attributes/cache_attribute_text_values';
 
@@ -18,6 +19,11 @@ class Configuration
     public function isAsyncOptionPricesEnabled(): bool
     {
         return $this->scopeConfig->isSetFlag(self::XML_PATH_SWATCHES_ASYNC_OPTION_PRICES, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+
+    public function isPreloadChildProductsImagesEnabled(): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_SWATCHES_PRELOAD_CHILD_PRODUCTS_IMAGES, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
     public function isCacheAttributeTextValuesEnabled(): bool
